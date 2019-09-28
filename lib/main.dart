5038d24b3d20b64799b1d3d4d6895eca981284d4
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-09-29 00:53:29 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:edu_app/Resources.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(title: TextStyle(fontFamily: "Nunito"))),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => Resources()));
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(right: 16.0, bottom: 32),
+          child: Container(
+            child: Text(
+              "Get Startted",
+              style: TextStyle(
+                  color: Color(0xffE1F5FF),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/splash.jpg"), fit: BoxFit.fill),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "World's Best",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color(0xffE1F5FF),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "Education  Resources",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 48,
+                    fontWeight: FontWeight.w900),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "Anytime, Anywhere, Easy to Learn",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color(0xffE1F5FF),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
